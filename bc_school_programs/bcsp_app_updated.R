b45000758be78203bfb56680f559783376067fa4
asanaiman <asanai@student.ubc.ca>
2022-12-09 15:31:20 -0800

    Changed the select box to a picker so there was the possibility of selecting multiple facility types

--- bc_school_programs/bcsp_app_updated.R
+++ bc_school_programs/bcsp_app_updated.R
@@ -2,6 +2,7 @@
 library("shiny")
 library("tidyverse")
 library("shinythemes")
+library("shinyWidgets")
 
 # Loading the Dataset
 bcsp <- read_csv("ProgramsOfferedinSchools.csv") %>% 
@@ -11,30 +12,33 @@ bcsp <- read_csv("ProgramsOfferedinSchools.csv") %>%
 # Making Shiny App
 ui <- fluidPage(
   theme = shinytheme("flatly"),
-  titlePanel("Programs Offered in B.C. Schools 2005-2014"),
+  titlePanel(strong("Programs Offered in B.C. Schools 2005-2014")),
   h5("This shiny app let's you explore the different programs offered in British Columbia Schools."),
   br(),
-  helpText("Filter your data by selecting the district range, school years, and facility type you want to look at:"),
+  helpText(strong("Filter your table by selecting the district range, school years, and facility type you want to look at:")),
   sidebarLayout(
     sidebarPanel(
       # Feature 1: Adding a slider that lets the user filter through the district numbers and view the different programs for either a single district or a range of districts.
       sliderInput("programs_slider", "Select a district range:", min = 5, max = 625, value = c(30, 65)),
       # Feature 2: Adding a check box group to allow user to filter and visualize wither a single school year or multiple.
       checkboxGroupInput("yearInput", "School Years:",
-                         choices = c("2005/2006", "2006/2007", "2007/2008", "2008/2009", "2009/2010", "2010/2011",
-                                     "2011/2012", "2012/2013", "2013/2014"),
+                         choices = unique(bcsp$`School Year`),
                          selected = c("2005/2006")),
-      # Feature 3: Adding a select box for facility type as it can be really difficult to filter through looking for specific facility input.
-      selectInput("facilityInput", "Facility Type:",
+      # Feature 3: Adding a picker for facility type as it can be really difficult to filter through looking for specific facility input. Picker input allows to select one or multiple options.
+      pickerInput("facilityInput", "Facility Type:",
                   c("Standard" = "STANDARD",
                     "Continuing ED" = "CONTINUING ED",
                     "Alternate" = "ALTERNATE",
                     "District Distance ED" = "DISTRICT DISTANCE ED",
-                    "Long Term PRP" = "LONG TERM PRP", 
-                    "Youth Custody" = "YOUTH CUSTODY")),
+                    "Long Term PRP" = "LONG TERM PRP",
+                    "Youth Custody" = "YOUTH CUSTODY"),
+                  selected = "STANDARD", 
+                  options = list(`actions-box` = TRUE, `none-selected-text` = ("Please Select Preferred Facility Type(s)")), 
+                  multiple = TRUE),
+      # Feature 4: Adding a submit button so the datatable is produced once the submit button is pressed.
       submitButton(text = "Apply Changes", icon = icon("refresh"), width = NULL),
-      helpText("Click the Apply Changes button for the filters you applied to be reflected in the table output.")),
-    # Feature 4: Adding a table that allows the user to view the different programs offered in BC based off of the filters they indicated in feature 1 and 2.
+      helpText("Click the", strong("Apply Changes"), "button for the filters you applied to be reflected in the table output.")),
+    # Feature 5: Adding a table that allows the user to view the different programs offered in BC based off of the filters they indicated in feature 1 and 2.
     mainPanel("bcsp", DT::dataTableOutput("programs_table"))))
 
 server <- function(input, output) {
